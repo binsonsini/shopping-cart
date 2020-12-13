9491a59400371f96104b0f3d899f951ccd1e6f3a
Sini Varghese <binsonsini@gmail.com>
2020-12-13 20:59:51 +0530

    commit shopping cart

---
+++ helpers/product-helpers.js
@@ -0,0 +1,11 @@
+var db=require('../config/connection');
+module.exports={
+    
+    addProduct:(product,callback)=>{
+        console.log(product);
+
+       // db.get.getCollection('product').insertOne(product).then((data)=>{
+      //      callback(true)
+        //})
+    }
+}
